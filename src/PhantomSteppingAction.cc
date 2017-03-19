5a9d896f3b702da94110229212f5598d957efc33
John Allison <John.Allison@g4ai.org>
2017-03-19 12:32:18 +0000

    Comment out body of user action

--- src/PhantomSteppingAction.cc
+++ src/PhantomSteppingAction.cc
@@ -24,28 +24,28 @@ PhantomSteppingAction::~PhantomSteppingAction()
 
 void PhantomSteppingAction::UserSteppingAction(const G4Step* step)
 {
-  // Does it enter the fibre?
-  G4StepPoint* postStepPoint = step->GetPostStepPoint();
-  if (postStepPoint->GetPhysicalVolume() != pFibre_phys) return;
-
-  // Is it an optical photon?
-  G4Track* track = step->GetTrack();
-  const G4ParticleDefinition* pPDef = track->GetParticleDefinition();
-  if (pPDef != G4OpticalPhoton::Definition()) return;
-
-  // It's an optical photon entering the fibre
-  G4StepPoint* preStepPoint = step->GetPreStepPoint();
-  const G4ThreeVector& incomingDirection = preStepPoint->GetMomentumDirection();
-  G4ThreeVector z_axis (0., 0., 1.);
-  if (incomingDirection * z_axis > 0.92388) {
-    fpRunAction->IncrementPhotonCount();
-    // Ask the tracking manager to kill it!!!
-    track->SetTrackStatus(fStopAndKill);
-  } else {
-    // What happens to photons not in acceptance cone?  Do they reflect off?
-    // Or should we kill these too?  Yes...kill for now
-    track->SetTrackStatus(fStopAndKill);
-  }
+//  // Does it enter the fibre?
+//  G4StepPoint* postStepPoint = step->GetPostStepPoint();
+//  if (postStepPoint->GetPhysicalVolume() != pFibre_phys) return;
+//
+//  // Is it an optical photon?
+//  G4Track* track = step->GetTrack();
+//  const G4ParticleDefinition* pPDef = track->GetParticleDefinition();
+//  if (pPDef != G4OpticalPhoton::Definition()) return;
+//
+//  // It's an optical photon entering the fibre
+//  G4StepPoint* preStepPoint = step->GetPreStepPoint();
+//  const G4ThreeVector& incomingDirection = preStepPoint->GetMomentumDirection();
+//  G4ThreeVector z_axis (0., 0., 1.);
+//  if (incomingDirection * z_axis > 0.92388) {
+//    fpRunAction->IncrementPhotonCount();
+//    // Ask the tracking manager to kill it!!!
+//    track->SetTrackStatus(fStopAndKill);
+//  } else {
+//    // What happens to photons not in acceptance cone?  Do they reflect off?
+//    // Or should we kill these too?  Yes...kill for now
+//    track->SetTrackStatus(fStopAndKill);
+//  }
 
   return;
 }
